990763e90df4ea966a3eca1971a1ca0879d13536
Kenneth Bunker <kabunker@gmail.com>
2025-04-02 16:27:07 -0300

    Update estadistica 2 - plantilla.R

--- estadistica2/data/estadistica 2 - plantilla.R
+++ estadistica2/data/estadistica 2 - plantilla.R
@@ -4,38 +4,32 @@
 # contacto  : kenneth.bunker@uss.cl
 # ramo      : Estadística II
 # trabajo   : Nombre de Trabajo
-#           
-#           
-#
-#
-#
+# permalink : https://github.com/kennethbunker/uss/tree/main/estadistica2/
 ###################################################
 
-## Limpiar el caché
+## Limpiar el caché (eliminar variables guardadas anteriormente)
 rm(list=ls())
-
-Sys.setlocale("LC_ALL", "ES_ES.UTF-8")
 
 ###################################################
 # DIRECTORIO DE TRABAJO
 ###################################################
 
-## CAMBIAR A TU COMPUTADOR
-#setwd("/Users/kennethbunker/Dropbox/USS/clases/2025-01/estadistica 2/solemne 3/")
+## Establecer directorio de trabajo en tu computador
+#setwd("/ruta/a/tu/directorio/")
 
 ###################################################
 # BIBLIOTECA
+# Cargar e instalar paquetes necesarios
 ###################################################
 
 install.packages("pacman")
-pacman::p_load(RCurl, ggplot2, ggplot2, car,
-               dplyr, plyr,readxl,writexl,openxlsx,
-               correlation,see,ggplot2)
+pacman::p_load(RCurl, ggplot2, car,
+               dplyr, plyr, readxl, writexl, openxlsx,
+               correlation, see)
 
 ###################################################
 # ABRIR DATOS BUNKER Y NEGRETTO
-#
-#
+# Cargar base de datos latam.csv desde Github
 ###################################################
 
 x <- getURL("https://raw.githubusercontent.com/kennethbunker/uss/main/estadistica2/data/latam.csv") 
@@ -43,8 +37,7 @@ byn <- read.csv(text = x)
 
 ###################################################
 # ABRIR DATOS V-DEM
-#
-#
+# Cargar base de datos vdem.csv desde Github
 ###################################################
 
 xx <- getURL("https://raw.githubusercontent.com/kennethbunker/uss/main/estadistica2/data/vdem.csv") 
@@ -52,8 +45,7 @@ vdem <- read.csv(text = xx)
 
 ###################################################
 # ABRIR DATOS POLARIZACION
-#
-#
+# Cargar base de datos polarizacion.csv desde Github
 ###################################################
 
 xxx <- getURL("https://raw.githubusercontent.com/kennethbunker/uss/main/estadistica2/data/polarizacion.csv") 
@@ -61,51 +53,47 @@ polarizacion <- read.csv(text = xxx, encoding="UTF-8")
 
 ###################################################
 # LIMPIAR BASE
-#   # sacar filas que tienen NA
-#   # 
-#
+# Remover filas que tienen valores faltantes (NA)
 ###################################################
 
-polarizacion <- drop_na(polarizacion)
+#polarizacion <- drop_na(polarizacion)
 
 ###################################################
-# DEFINICIÓN DE VARIABLES <------------------------------------------- ingresa tus variables aquí!!!
-#   # elegir base y variables [ejemplo: "base$variable"]
-#   # si están bien ingresadas, no hay que tocar nada abajo
-#
+# DEFINICIÓN DE VARIABLES
+# Define aquí tus variables usando formato "base$variable"
+# Si las defines correctamente, no es necesario modificar abajo
 ###################################################
 
-## asignar nombre genérico a mis variables
-vd  <- polarizacion$boric_1v2021
-vi1 <- polarizacion$coord1D_normal_all
-vi2 <- polarizacion$edad
-vi3 <- polarizacion$magnitud_distrital
+## Variables para análisis (ajustar si es necesario)
+vd  <- polarizacion$boric_1v2021                 # variable dependiente
+vi1 <- polarizacion$coord1D_normal_all           # variable independiente 1
+vi2 <- polarizacion$edad                         # variable independiente 2
+vi3 <- polarizacion$magnitud_distrital           # variable independiente 3
 
 ###################################################
 # EXPLORAR DATOS (TABLAS)
-#
-#
+# Estadísticos descriptivos (media, desviación estándar, mínimo y máximo)
 ###################################################
 
-## mi variable dependiente
+## Estadísticas variable dependiente
 mean(vd, na.rm = T)
 sd(vd, na.rm = T)
 min(vd, na.rm = T)
 max(vd, na.rm = T)
 
-## mi variable independiente 1
+## Estadísticas variable independiente 1
 mean(vi1, na.rm = T)
 sd(vi1, na.rm = T)
 min(vi1, na.rm = T)
 max(vi1, na.rm = T)
 
-## mi variable independiente 2
+## Estadísticas variable independiente 2
 mean(vi2, na.rm = T)
 sd(vi2, na.rm = T)
 min(vi2, na.rm = T)
 max(vi2, na.rm = T)
 
-## mi variable independiente 3
+## Estadísticas variable independiente 3
 mean(vi3, na.rm = T)
 sd(vi3, na.rm = T)
 min(vi3, na.rm = T)
@@ -113,11 +101,9 @@ max(vi3, na.rm = T)
 
 ###################################################
 # EXPLORAR DATOS (BOXPLOT)
-#
-#
+# Gráficos tipo boxplot para identificar valores extremos
 ###################################################
 
-## boxplot
 boxplot(vd, main="Variable Dependiente")
 boxplot(vi1, main="Variable Independiente 1")
 boxplot(vi2, main="Variable Independiente 2")
@@ -125,11 +111,9 @@ boxplot(vi3, main="Variable Independiente 3")
 
 ###################################################
 # EXPLORAR DATOS (DENSIDADES)
-#
-#
+# Gráficos de densidad para evaluar la distribución
 ###################################################
 
-## distribuciones de densidades
 dens1 <- density(vd, na.rm = T)
 plot(dens1, main="Densidad: Variable Dependiente")
 
@@ -144,95 +128,86 @@ plot(dens4, main="Densidad: Variable Independiente 3")
 
 ###################################################
 # CORRELACIÓN
-#
-#
+# Calcular correlación y graficar relación entre variables
 ###################################################
 
-# esta es mi primera correlación
+# Correlación entre variables
 cor(vd, vi1, use="pairwise.complete.obs")
 
-# este es mi primer gráfico de asociación
+# Gráfico de dispersión entre variables
 plot(vi1, vd, 
      main="Relación entre Variables",
-     ylab="Nombre Variable Dependiente",
-     xlab="Nombre Variable Independiente")
+     ylab="Variable Dependiente",
+     xlab="Variable Independiente")
 
 ###################################################
 # REGRESIÓN
-#
-#
+# Modelos de regresión lineal simple y múltiple
 ###################################################
 
-## este es mi modelo 1 (tiene la principal variable independiente)
+## Modelo 1: Regresión simple (vd ~ vi1)
 modelo1 <- lm(vd ~ vi1)
 summary(modelo1)
-nobs(modelo1) # nobs = numero de observaciones = N
+nobs(modelo1)
 
-## este es mi modelo 2 (tiene una variable independiente)
+## Modelo 2: Regresión simple (vd ~ vi2)
 modelo2 <- lm(vd ~ vi2)
 summary(modelo2)
-nobs(modelo2) # nobs = numero de observaciones = N
+nobs(modelo2)
 
-## este es mi modelo 3 (tiene otra variable independiente)
+## Modelo 3: Regresión simple (vd ~ vi3)
 modelo3 <- lm(vd ~ vi3)
 summary(modelo3)
-nobs(modelo3) # nobs = numero de observaciones = N
+nobs(modelo3)
 
-## este es mi modelo 4 (tiene dos variables independientes)
+## Modelo 4: Regresión múltiple (vd ~ vi1 + vi3)
 modelo4 <- lm(vd ~ vi1 + vi3)
 summary(modelo4)
-nobs(modelo4) # nobs = numero de observaciones = N
+nobs(modelo4)
 
-## este es mi modelo 5 (tiene dos variables independientes)
+## Modelo 5: Regresión múltiple (vd ~ vi1 + vi2)
 modelo5 <- lm(vd ~ vi1 + vi2)
 summary(modelo5)
-nobs(modelo5) # nobs = numero de observaciones = N
+nobs(modelo5)
 
-## este es mi modelo 6 (tiene tres variables independientes)
+## Modelo 6: Regresión múltiple (vd ~ vi1 + vi2 + vi3)
 modelo6 <- lm(vd ~ vi1 + vi2 + vi3)
 summary(modelo6)
-nobs(modelo6) # nobs = numero de observaciones = N
+nobs(modelo6)
 
 ###################################################
 # REGRESIÓN + LINEA DE TENDENCIA
-#
-#
+# Gráficos con líneas de regresión ajustadas
 ###################################################
 
 plot(vd ~ vi1, 
-     main="Título del Gráfico",
-     ylab="Nombre Variable Dependiente",
-     xlab="Nombre Variable Independiente")
+     main="Variable Dependiente vs Independiente 1",
+     ylab="Variable Dependiente",
+     xlab="Variable Independiente 1")
 abline(modelo1, col="red")
 
 plot(vd ~ vi2, 
-     main="Título del Gráfico",
-     ylab="Nombre Variable Dependiente",
-     xlab="Nombre Variable Independiente")
+     main="Variable Dependiente vs Independiente 2",
+     ylab="Variable Dependiente",
+     xlab="Variable Independiente 2")
 abline(modelo2, col="red")
 
 plot(vd ~ vi3, 
-     main="Título del Gráfico",
-     ylab="Nombre Variable Dependiente",
-     xlab="Nombre Variable Independiente")
+     main="Variable Dependiente vs Independiente 3",
+     ylab="Variable Dependiente",
+     xlab="Variable Independiente 3")
 abline(modelo3, col="red")
 
+# ###################################################
+# OTROS EJEMPLOS: 1
+# Gráficos de valores añadidos (partial regression plots)
+###################################################
+# car::avPlots(modelo1)
 
 # ###################################################
-# # OTROS EJEMPLOS: 1
-# #
-# #
-# ###################################################
-# 
-# car::avPlots(modelo1)
-# 
-# ###################################################
-# # OTROS EJEMPLOS: 2
-# # # hay que definir base y variables
-# #
-# ###################################################
-# 
-# ## una correlación de arcoiris
+# OTROS EJEMPLOS: 2
+# Gráfico avanzado de correlación con paquete see
+###################################################
 # result <- cor_test(byn, "dep_dm", "enpv_bn")
 # plot(result,
 #      point = list(
@@ -244,13 +219,3 @@ abline(modelo3, col="red")
 #   see::theme_modern() +
 #   see::scale_color_material_c(palette = "rainbow", guide = "none") +
 #   scale_size_continuous(guide = "none")
-# 
-# ###################################################
-# # OTROS EJEMPLOS: 3
-# #
-# #
-# ###################################################
-# 
-# 
-# 
-# 
